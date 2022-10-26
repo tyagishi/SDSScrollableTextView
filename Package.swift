0ed3980cf48c528ce49e0cccd94a69bddb94dab9
Tomoaki Yagishita <tyagishi@gmail.com>
2022-10-26 12:55:56 +0900

    update dependency

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSNSUIBridge", branch: "main"),
+        .package(url: "https://github.com/tyagishi/SDSStringExtension", branch: "main"),
         .package(url: "https://github.com/tyagishi/SwiftUIDebugUtil", .upToNextMajor(from: .init(2, 0, 0)))
     ],
     targets: [
@@ -26,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDSScrollableTextView",
-            dependencies: ["SDSNSUIBridge", "SwiftUIDebugUtil"]),
+            dependencies: ["SDSNSUIBridge", "SDSStringExtension", "SwiftUIDebugUtil"]),
         .testTarget(
             name: "SDSScrollableTextViewTests",
             dependencies: ["SDSScrollableTextView"]),
